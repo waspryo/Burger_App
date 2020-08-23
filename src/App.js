ff780ef7b459f0eb45441645a7a606885993d6dd
ldl093 <sugimoto@liddell.tokyo>
2020-08-23 23:53:04 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from './components/Layout/Layout';
+
+function App() {
+  return (
+    <div>
+      <Layout >
+        <p>テスト</p>
+      </Layout>
+    </div>
+  );
+}
+
+export default App;
