f173b00c9415857c51717bdaf0dd80b9d7e23625
ldl093 <sugimoto@liddell.tokyo>
2020-09-03 23:09:30 +0900

    Adding a Dynamic Ingredient Component

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Aux from '../../hoc/Aux';
+
+class BugerBuiler extends Component {
+  render() {
+    return (
+      <Aux>
+        <div>Buger</div>
+        <div>Build Controls</div>
+      </Aux>
+    )
+  }
+}
+export default BugerBuiler
